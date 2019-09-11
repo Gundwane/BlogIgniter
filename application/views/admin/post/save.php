6750a5eb6564cf0d79fa415f83b0fbc84ab9344e
Eze Morales <ezhemorales@hotmail.com>
2019-09-11 17:40:56 -0300

    Solucionado error con JQuery

--- application/views/admin/post/save.php
+++ application/views/admin/post/save.php
@@ -156,6 +156,7 @@
 
 
         <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/jquery-3.4.1.min.js"></script>
         <!-- AdminLTE App -->
         <script src="<?php echo base_url(); ?>assets/js/admin/adminlte.min.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/admin/demo.js"></script>
