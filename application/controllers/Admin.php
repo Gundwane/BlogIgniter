901a7bc3f5969856a28f02efd77773297b9c89a8
Eze Morales <ezhemorales@hotmail.com>
2019-09-20 17:39:15 -0300

    Añadido post

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,6 +9,7 @@ class Admin extends CI_Controller
         $this->load->helper("form");
         $this->load->helper("Post_helper");
         $this->load->library("parser");
+        $this->load->library("Form_validation");
     }
 
     public function index()
@@ -25,6 +26,28 @@ class Admin extends CI_Controller
 
     public function post_save()
     {
+        if($this->input->server("REQUEST_METHOD") == "POST")
+        {
+            $this->form_validation->set_rules('title', 'Titulo', 'required|min_length[10]|max_length[65]');
+            $this->form_validation->set_rules('content', 'Contenido', 'required|min_length[10]');
+            $this->form_validation->set_rules('description', 'Descripción', 'max_length[100]');
+            $this->form_validation->set_rules('posted', '', 'required');
+
+            if($this->form_validation->run())
+            {
+                $save = array
+                (
+                    'title' => $this->input->post("title"),
+                    'content' => $this->input->post("content"),
+                    'description' => $this->input->post("description"),
+                    'posted' => $this->input->post("posted")
+                );
+
+                $post_id = $this->Post->insert($save);
+
+            }
+        }
+
         $data["data_posted"] = posted();
         $view["title"] = "Crear Post";
         $view["body"] = $this->load->view('admin/post/save', $data, TRUE);
